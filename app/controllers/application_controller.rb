5f7083ed3a11b9b2e9a87f307a0c45277cce0902
itaru.nakaya <george_h_taro@yahoo.co.jp>
2020-06-30 07:35:21 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,11 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_url
     end
-  end  
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+  end
 end
